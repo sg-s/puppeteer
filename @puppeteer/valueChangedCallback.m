b1d6aa3f54b4a5b0640ff62e06f79134e8ab44a0
Srinivas Gorur-Shandilya <code@srinivas.gs>
2020-04-03 15:30:56 -0400

    added functionality to valueChangedCallback

--- @puppeteer/valueChangedCallback.m
+++ @puppeteer/valueChangedCallback.m
@@ -1,1 +1,25 @@
 function valueChangedCallback(self,src,event)
+
+
+idx = find(self.handles.sliders == src);
+
+
+
+% update the parameter values
+self.parameter_values(idx) = event.Value;
+
+% update the corresponding control label
+this_string = self.handles.controllabel(idx).Text;
+this_string = this_string(1:strfind(this_string,'='));
+this_string = [this_string strlib.oval(event.Value)];
+self.handles.controllabel(idx).Text = this_string;
+
+
+if isempty(self.valueChangingFcn)
+	return
+end
+
+self.valueChangedFcn(self.parameter_names(idx),self.parameter_values(idx))
+
+
+
